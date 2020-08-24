2fd21a074470aaf71eda50f678667affc28329c2
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-08-24 03:38:12 +0100

    ssh

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # Set up your client SSH configuration file
 exec {'config':
-command  => 'echo "    IdentityFile ~/.ssh/holberton\n    ForwardAgent yes" >> /etc/ssh/ssh_config',
+command  => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
 path     => '/bin',
 }
