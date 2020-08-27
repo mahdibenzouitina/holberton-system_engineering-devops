72a6ad2978eef7706c8d0fc2929643b2421c67bb
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-08-27 02:49:18 +0100

    install nginx using puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,7 +2,20 @@
 exec {'install ngix':
     provider => shell,
     command => 'sudo apt- get -y update',
+}
+
+exec {'install ngix':
+    provider => shell,
     command => 'sudo apt-get -y install nginx ; echo "Holberton School" | sudo tee /var/www/html/index.html',
+}
+
+exec {'install ngix':
+    provider => shell,
     command => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/pasignature permanent;/" /etc/nginx/sites-available/default',
+
+}
+
+exec {'install ngix':
+    provider => shell,
     command => 'sudo service nginx start',
 }
