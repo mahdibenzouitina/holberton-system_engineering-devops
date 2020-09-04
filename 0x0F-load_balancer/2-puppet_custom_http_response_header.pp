a06f2657fdb8db38e163788fcf0a4ecc8e3bfa25
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-09-04 13:50:35 +0100

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,11 +2,10 @@
 #custom HTTP header response, but with Puppet.
 
 exec {'HAproxy':
-command  => 'sudo apt-get -y update &&
-sudo apt-get -y install nginx &&
-hostname=$(cat /etc/hostname) &&
-file='/etc/nginx/nginx.conf' &&
-sudo sed -i "48 i\ \tadd_header X-Served-By $hostname;\n" /etc/nginx/sites-enabled/default &&
-sudo service nginx restart',
+command  => 'hostname=$(cat /etc/hostname) &&
+	        sudo apt-get -y update && 
+            sudo apt-get -y install nginx && 
+            sudo sed -i "40i \\\tadd_header X-Served-By \$hostname;\n" /etc/nginx/sites-enabled/default &&
+            sudo service nginx restart',',
 provider => shell,
 }
