f75a63a55041312325878acf759a5d0e10d6a7e3
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-09-04 13:52:09 +0100

    puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,6 +6,6 @@ command  => 'hostname=$(cat /etc/hostname) &&
 	        sudo apt-get -y update && 
             sudo apt-get -y install nginx && 
             sudo sed -i "40i \\\tadd_header X-Served-By \$hostname;\n" /etc/nginx/sites-enabled/default &&
-            sudo service nginx restart',',
+            sudo service nginx restart',
 provider => shell,
 }
