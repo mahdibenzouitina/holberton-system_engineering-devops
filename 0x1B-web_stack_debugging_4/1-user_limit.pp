59240e7f3cdacd82faa3450ec2e06c11d8c57b71
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-10-09 22:39:45 +0100

    debugging

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,7 @@
 #web stack debugging
 exec {'holberton soft nofile':
 path     => ['/usr/bin/'],
-command  => "sudo sed -i 's/holberton soft nofile 4/soft nofile 1997/$
+command  => "sudo sed -i 's/holberton soft nofile 4/soft nofile 1997/g' /etc/security/limits.conf",
 provider => 'shell',
 }
 exec {'holberton hard no file':
